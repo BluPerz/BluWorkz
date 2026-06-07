55854db9c092933193fcef1c2a108817064d2b65
BluPerz <amprio049@gmail.com>
2026-06-07 15:39:01 +0800

    Delete aq_1_real directory

--- aq_1_real/script/del_file.py
+++
@@ -1,7 +0,0 @@
-import os
-
-if os.path.exists("temporary.txt"):
-    os.remove("temporary.txt")
-    print("File deleted successfully.")
-else:
-    print("File does not exist.")
--- aq_1_real/script/upd_file.py
+++
@@ -1,5 +0,0 @@
-file = open("notes.txt", "a")
-file.write("\nUpdating files is easy and useful.")
-file.close()
-
-print("Line added successfully.")
